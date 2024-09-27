57504fb0e7193a91d264aee8b9170316daa6d8f9
Austin <austinematurure@gmail.com>
2024-09-27 13:58:42 +0300

    Push to merge

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
           <h1>Make Every Talk Effortless,</h1>
           <h1>One Card at a Time</h1>
         </div>
-        <hr />
+        <hr color="#31603d" />
       </section>
     </>
   );
