a140b2a6f8cd9fa16f39b88e01c1df381d2e0571
Austin <austinematurure@gmail.com>
2024-10-09 23:48:38 +0300

    Added Tutorial account Page

---
+++ Frontend/src/components/Tutorials.jsx
@@ -0,0 +1,16 @@
+function Tutorial() {
+  return (
+    <>
+      <div className="tutorial-text">
+        <h1>Tutorials,</h1>
+        <h1>
+          Learn how to get the best out of Talk assist i.e the best out of your
+          talk
+        </h1>
+        <h3>Coming Soon.</h3>
+      </div>
+    </>
+  );
+}
+
+export default Tutorial;
