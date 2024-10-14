d1429e912733f9ef0e802c23ba010d1ed65590d9
Austin <austinematurure@gmail.com>
2024-10-14 20:40:20 +0300

    Added regex logic to seperate dialog appropiatly

--- Backend/textapi/views.py
+++ Backend/textapi/views.py
@@ -3,8 +3,14 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import spacy
-# Load spaCy English model
+import re
+
 nlp = spacy.load("en_core_web_sm")
+
+
+
+
+
 
 @csrf_exempt
 def divide_text(request):
@@ -17,13 +23,25 @@ def divide_text(request):
                 return JsonResponse({'error': 'No text provided'}, status=400)
 
       
-            doc = nlp(text)
-            sentences = [sent.text.strip() for sent in doc.sents]
-            divided_text = "# ".join(sentences)
+           
+            dialogue_pattern = r"([A-Za-z]+:\s.*?)(?=\s*[A-Za-z]+:|$)"
+            
+           
+            if re.search(dialogue_pattern, text):
+               
+                dialogues = re.findall(dialogue_pattern, text, re.DOTALL)
+       
+                divided_text = "# ".join([dialogue.strip() for dialogue in dialogues])
+            else:
+                
+                doc = nlp(text)
+                sentences = [sent.text.strip() for sent in doc.sents]
+                divided_text = "# ".join(sentences)
 
             return JsonResponse({'dividedText': divided_text}, status=200)
 
         except Exception as e:
             print(f"Error: {e}")
             return JsonResponse({'error': str(e)}, status=500)
+    
     return JsonResponse({'error': 'Invalid request'}, status=400)
